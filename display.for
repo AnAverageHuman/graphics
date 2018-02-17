43a9c0dbdea14b462c8f4ba9eb088515f6edbb7b
Jeffrey Lin <jeffrey@icurse.nl>
2018-02-17 00:06:56 -0500

    display.for: add subroutine for plotting points

--- display.for
+++ display.for
@@ -3,7 +3,7 @@
 
           IMPLICIT NONE
           PRIVATE
-          PUBLIC :: DISPLAY_PRINT
+          PUBLIC :: DISPLAY_PRINT, PLOT
       CONTAINS
           SUBROUTINE DISPLAY_PRINT(DISPLAY)
               INTEGER, INTENT(IN) :: DISPLAY(:, :, :, :)
@@ -11,5 +11,13 @@
               WRITE(*, "(A)") MAGICNUMBER  ! suppress beginning space
               WRITE(*, *) DIMR, DIMC, MAXCOLOR, DISPLAY
           END SUBROUTINE DISPLAY_PRINT
+
+          PURE SUBROUTINE PLOT(DISPLAY, X, Y, Z, COLOR)
+              INTEGER, INTENT(INOUT), DIMENSION(:, :, :, :) :: DISPLAY
+              INTEGER, INTENT(IN) :: X, Y, Z
+              INTEGER, INTENT(IN), DIMENSION(3) :: COLOR
+
+              DISPLAY(:, Z, Y, X) = COLOR
+          END SUBROUTINE PLOT
       END MODULE DISPLAY
 
