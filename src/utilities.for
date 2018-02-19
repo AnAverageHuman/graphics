7fa28784c31ad04da7b17127b61586295b332908
Jeffrey Lin <jeffrey@icurse.nl>
2018-02-19 00:20:04 -0500

    utilities.for: add function for coercing to string

--- src/utilities.for
+++ src/utilities.for
@@ -1,7 +1,7 @@
       MODULE UTILITIES
           IMPLICIT NONE
           PRIVATE
-          PUBLIC :: STDERR
+          PUBLIC :: STDERR, TOSTR
       CONTAINS
           SUBROUTINE STDERR(MESSAGE)
               USE ISO_FORTRAN_ENV, ONLY : ERROR_UNIT
@@ -9,5 +9,13 @@
 
               WRITE(ERROR_UNIT, '(A)') MESSAGE
           END SUBROUTINE STDERR
+
+          PURE FUNCTION TOSTR(K) RESULT(STR)
+              INTEGER, INTENT(IN) :: K
+              CHARACTER(LEN=20) :: STR
+
+              WRITE(STR, *) K
+              STR = ADJUSTL(STR)
+          END FUNCTION TOSTR
       END MODULE UTILITIES
 
