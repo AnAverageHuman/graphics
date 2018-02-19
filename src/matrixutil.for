df188e8a56410ac4c9c767b9f2f388537c6cb28a
Jeffrey Lin <jeffrey@icurse.nl>
2018-02-19 00:09:17 -0500

    matrixutil.for: add subroutine for printing matrix

---
+++ src/matrixutil.for
@@ -0,0 +1,14 @@
+      MODULE MATRIXUTIL
+          IMPLICIT NONE
+          PRIVATE
+          PUBLIC :: MATRIX_PRINT
+      CONTAINS
+          SUBROUTINE MATRIX_PRINT(MATRIX)
+              INTEGER, INTENT(IN) :: MATRIX(:, :)
+              INTEGER :: ROW, COL
+              DO ROW=1, SIZE(MATRIX, 2)
+                  WRITE (*,*) (MATRIX(COL, ROW), COL=1, SIZE(MATRIX, 1))
+              ENDDO
+          END SUBROUTINE MATRIX_PRINT
+      END MODULE MATRIXUTIL
+
