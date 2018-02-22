73585253658d1368a404bfa6c201d34106c6f51f
Jeffrey Lin <jeffrey@icurse.nl>
2018-02-22 02:03:56 -0500

    matrixutil.for: generate identity matrix

--- src/matrixutil.for
+++ src/matrixutil.for
@@ -1,7 +1,7 @@
       MODULE MATRIXUTIL
           IMPLICIT NONE
           PRIVATE
-          PUBLIC :: MATRIX_PRINT
+          PUBLIC :: MATRIX_PRINT, MATRIX_IDENT
       CONTAINS
           SUBROUTINE MATRIX_PRINT(MATRIX)
               INTEGER, INTENT(IN) :: MATRIX(:, :)
@@ -10,5 +10,13 @@
                   WRITE (*,*) (MATRIX(COL, ROW), COL=1, SIZE(MATRIX, 1))
               ENDDO
           END SUBROUTINE MATRIX_PRINT
+
+          PURE SUBROUTINE MATRIX_IDENT(M)
+              INTEGER, INTENT(INOUT) :: M(:, :)
+              INTEGER :: I
+
+              M(:, :) = 0
+              FORALL(I = 1:MIN(SIZE(M, 1), SIZE(M, 2))) M(I, I) = 1
+          END SUBROUTINE MATRIX_IDENT
       END MODULE MATRIXUTIL
 
