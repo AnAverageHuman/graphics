31a8a9a278dc9b3ff0324a02c69787cce2782aa8
Jeffrey Lin <jeffrey@icurse.nl>
2018-02-22 23:49:17 -0500

    edgematrix.for: draw the contents of edge matrices

--- src/edgematrix.for
+++ src/edgematrix.for
@@ -12,6 +12,7 @@
           CONTAINS
               PROCEDURE, NON_OVERRIDABLE :: ADDPOINT
               PROCEDURE, NON_OVERRIDABLE :: ADDEDGE
+              PROCEDURE, NON_OVERRIDABLE :: DRAW
           END TYPE EDGMAT
       CONTAINS
           SUBROUTINE ADDPOINT(THIS, DATA)
@@ -33,5 +34,26 @@
               CALL THIS%ADDPOINT(D1)
               CALL THIS%ADDPOINT(D2)
           END SUBROUTINE ADDEDGE
+
+          SUBROUTINE DRAW(THIS, DISPLAY, COLOR)
+              CLASS(EDGMAT), INTENT(IN)    :: THIS
+              INTEGER,       INTENT(INOUT) :: DISPLAY(:, :, :, :)
+              INTEGER,       INTENT(IN)    :: COLOR(3)
+              CLASS(NODE), POINTER  :: CURR
+              INTEGER, DIMENSION(3) :: P1, P2
+
+              CURR => THIS%FIRST
+              DO WHILE (ASSOCIATED(CURR%GETNEXT()))
+                  ! TODO: make this block look prettier
+                  SELECT TYPE (X => CURR%GETVAL())
+                      TYPE IS (INTEGER); P1 = X
+                  END SELECT
+                  SELECT TYPE (X => CURR%NEXT%GETVAL())
+                      TYPE IS (INTEGER); P2 = X
+                  END SELECT
+                  CALL DRAWLINE(DISPLAY, P1, P2, COLOR)
+                  CURR => CURR%GETNEXT()
+              END DO
+          END SUBROUTINE DRAW
       END MODULE EDGEMATRIX
 
