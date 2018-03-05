941571ae1629708c128cba9f15f60e97116efd9f
Jeffrey Lin <jeffrey@icurse.nl>
2018-03-04 23:20:04 -0500

    utilities.for: add degree to radian subroutine

--- src/utilities.for
+++ src/utilities.for
@@ -1,7 +1,13 @@
       MODULE UTILITIES
+          USE CONFIG
+
           IMPLICIT NONE
           PRIVATE
-          PUBLIC :: STDERR, TOSTR
+          PUBLIC :: STDERR, TOSTR, DEG2RAD
+
+          REAL, PARAMETER :: PI     = 4._DP * ATAN(1._DP)
+          REAL, PARAMETER :: DEG180 = 180._DP
+          REAL, PARAMETER :: D_TO_R = PI / DEG180
       CONTAINS
           SUBROUTINE STDERR(MESSAGE)
               USE ISO_FORTRAN_ENV, ONLY : ERROR_UNIT
@@ -17,5 +23,12 @@
               WRITE(STR, *) K
               STR = ADJUSTL(STR)
           END FUNCTION TOSTR
+
+          PURE FUNCTION DEG2RAD(D) RESULT(R)
+              REAL(DP), INTENT(IN) :: D
+              REAL(DP) :: R
+
+              R = D * D_TO_R
+          END FUNCTION DEG2RAD
       END MODULE UTILITIES
 
