0bf25cab8afb2c3cc59773cf0f3d73397f46b48a
Jeffrey Lin <jeffrey@icurse.nl>
2018-03-04 23:38:29 -0500

    matrixutil.for: add "three"-dimensional rotations

--- src/matrixutil.for
+++ src/matrixutil.for
@@ -4,7 +4,8 @@
           IMPLICIT NONE
           PRIVATE
           PUBLIC :: MATRIX_PRINT, MATRIX_IDENT, MATRIX_TRANSLATE,
-     +              MATRIX_SCALE, MATRIX_MULT
+     +              MATRIX_SCALE, MATRIX_ROTX, MATRIX_ROTY, MATRIX_ROTZ,
+     +              MATRIX_MULT
       CONTAINS
           SUBROUTINE MATRIX_PRINT(MATRIX)
               REAL(DP), INTENT(IN) :: MATRIX(:, :)
@@ -41,6 +42,39 @@
                   M(I, I) = M(I, I) * S(I)
               END FORALL
           END SUBROUTINE MATRIX_SCALE
+
+          PURE SUBROUTINE MATRIX_ROTX(M, S)
+              REAL(DP), INTENT(INOUT) :: M(:, :)
+              REAL(DP), INTENT(IN)    :: S
+
+              CALL MATRIX_IDENT(M)
+              M(2, 2) =  COS(S)
+              M(2, 3) = -SIN(S)
+              M(3, 2) =  SIN(S)
+              M(3, 3) =  COS(S)
+          END SUBROUTINE MATRIX_ROTX
+
+          PURE SUBROUTINE MATRIX_ROTY(M, S)
+              REAL(DP), INTENT(INOUT) :: M(:, :)
+              REAL(DP), INTENT(IN)    :: S
+
+              CALL MATRIX_IDENT(M)
+              M(1, 1) =  COS(S)
+              M(3, 1) = -SIN(S)
+              M(1, 3) =  SIN(S)
+              M(3, 3) =  COS(S)
+          END SUBROUTINE MATRIX_ROTY
+
+          PURE SUBROUTINE MATRIX_ROTZ(M, S)
+              REAL(DP), INTENT(INOUT) :: M(:, :)
+              REAL(DP), INTENT(IN)    :: S
+
+              CALL MATRIX_IDENT(M)
+              M(1, 1) =  COS(S)
+              M(1, 2) = -SIN(S)
+              M(2, 1) =  SIN(S)
+              M(2, 2) =  COS(S)
+          END SUBROUTINE MATRIX_ROTZ
 
           PURE FUNCTION MATRIX_MULT(A, B) RESULT(C)
               !   A   n x m   SIZE(A, 2), SIZE(A, 1)
